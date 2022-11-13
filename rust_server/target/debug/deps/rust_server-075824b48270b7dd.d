64bbf40c3fa4ae0b94394be62632e797ba2ce272
jongwon <54282927+loosie@users.noreply.github.com>
2022-11-13 18:24:50 +0900

    hello rust again

---
+++ rust_server/target/debug/deps/rust_server-075824b48270b7dd.d
@@ -0,0 +1,5 @@
+/Users/ijong-won/Desktop/loosie_library/taste_rust/rust_server/target/debug/deps/rust_server-075824b48270b7dd: src/main.rs
+
+/Users/ijong-won/Desktop/loosie_library/taste_rust/rust_server/target/debug/deps/rust_server-075824b48270b7dd.d: src/main.rs
+
+src/main.rs:
---
+++ rust_server/target/debug/rust_server.d
@@ -0,0 +1,1 @@
+/Users/ijong-won/Desktop/loosie_library/taste_rust/rust_server/target/debug/rust_server: /Users/ijong-won/Desktop/loosie_library/taste_rust/rust_server/src/main.rs
